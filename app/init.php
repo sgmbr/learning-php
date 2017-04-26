6d7b361ad9a838b609d674b4b2b312bace30543e
sgmbr <satoshi.sgmr@gmail.com>
2017-04-27 01:55:24 +1200

    Modify DB connection to Heroku

--- app/init.php
+++ app/init.php
@@ -5,10 +5,17 @@ error_reporting(E_ALL);
 session_start();
 $_SESSION['user_id'] = 5;
 
+/* local settings
 $host = 'localhost';
 $dbname = 'todo';
 $user = 'root';
 $pass = 'root';
+*/
+
+$host = 'us-cdbr-iron-east-03.cleardb.net';
+$dbname = 'heroku_3344723f0824080';
+$user = 'b86c900f759671';
+$pass = 'ce394596';
 
 $dsn = "mysql:host=$host;dbname=$dbname";
 
